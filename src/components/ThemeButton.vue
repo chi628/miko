4b71de52238aa4c504acd8b507a0259ae6baaa2f
chi628 <miko82290@gmail.com>
2023-09-21 02:30:29 +0800

    init project

---
+++ src/components/ThemeButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed, onMounted, ref } from "vue"
+const isDarkMode = ref(false)
+import DarkIcon from "../images/moon.png"
+import LightIcon from "../images/sun.png"
+
+const icon = computed(() => {
+  return isDarkMode.value ? DarkIcon : LightIcon
+})
+
+onMounted(() => {
+  if (window.matchMedia) {
+    isDarkMode.value = window.matchMedia("(prefers-color-scheme: dark)").matches
+  }
+})
+</script>
+<template>
+  <div class="w-10 h-10 rounded-full cursor-pointer" @click="isDarkMode = !isDarkMode">
+    <img :src="icon.src" alt="" />
+  </div>
+</template>
